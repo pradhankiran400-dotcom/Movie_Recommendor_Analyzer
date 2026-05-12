6ce29a0e8f37736255952e07f094b07285c74302
pradhankiran400-dotcom <pradhankiran400@gmail.com>
2026-05-12 21:33:35 +0530

    Update app.py

--- Movie_Recommendor/app.py
+++ Movie_Recommendor/app.py
@@ -1,16 +1,14 @@
 import streamlit as st
-
 import gdown
 import os
 
 if not os.path.exists('similarity.pkl'):
-    url = 'https://drive.google.com/file/d/196lfVcfpm4e9XEuISwOV9XH_pzzobZon/view?usp=sharing'
-    gdown.download(url, 'similarity.pkl', quiet=False)
+    url = 'https://drive.google.com/uc?id=196lfVcfpm4e9XEuISwOV9XH_pzzobZon'
+    gdown.download(url, 'similarity.pkl', quiet=False, fuzzy=True)
 
 st.set_page_config(page_title="Movie Recommender", layout="wide")
-
 pg = st.navigation([
-        st.Page("pages/home.py", title="🎬 Home"),
-        st.Page("pages/analysis.py", title="📊 Analysis")
+    st.Page("pages/home.py", title="🎬 Home"),
+    st.Page("pages/analysis.py", title="📊 Analysis")
 ])
 pg.run()
